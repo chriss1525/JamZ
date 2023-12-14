e627424f93c31aade033f9c1aa31173115ff51ad
chriss1525 <okothchris15@gmail.com>
2023-12-14 11:36:42 +0300

    add responses for / and /api

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const app = express();
 const port = 3000;
 
 app.use(routes);
+app.get('/', (req, res) => res.send('Jamz is running!'));
+app.get('/api/', (req, res) => res.send('Jamz API is running!'));
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
 
